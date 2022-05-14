6fe4f4b5c3827ef2b3a5469f2fc0e91c8353e3ab
Martin31110 <hernandezmartin3110@gmail.com>
2022-05-13 19:50:51 -0500

    Menu pricipal diseño terminado (con posibles modificaciones)

--- Victorito_Halley/src/PVictorito/Menu_Principal.java
+++ Victorito_Halley/src/PVictorito/Menu_Principal.java
@@ -4,6 +4,7 @@ public class Menu_Principal extends javax.swing.JFrame {
 
     public Menu_Principal() {
         initComponents();
+        setLocationRelativeTo(null);
     }
 
     @SuppressWarnings("unchecked")
@@ -20,7 +21,11 @@ public class Menu_Principal extends javax.swing.JFrame {
         L7 = new javax.swing.JLabel();
         L8 = new javax.swing.JLabel();
         L9 = new javax.swing.JLabel();
-        jPanel2 = new javax.swing.JPanel();
+        L10 = new javax.swing.JLabel();
+        L11 = new javax.swing.JLabel();
+        L13 = new javax.swing.JLabel();
+        L14 = new javax.swing.JLabel();
+        L12 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -32,13 +37,14 @@ public class Menu_Principal extends javax.swing.JFrame {
         jPanel1.add(L1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 140, 20));
 
         L2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Icono.png"))); // NOI18N
-        jPanel1.add(L2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 60, 50, 40));
+        jPanel1.add(L2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 40, 50, 40));
 
         L3.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
         L3.setText("MENU PRINCIPAL");
-        jPanel1.add(L3, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 70, 120, 20));
+        jPanel1.add(L3, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 50, 120, 20));
 
         L4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Añadir cliente.PNG"))); // NOI18N
+        L4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         L4.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 L4MouseEntered(evt);
@@ -47,9 +53,10 @@ public class Menu_Principal extends javax.swing.JFrame {
                 L4MouseExited(evt);
             }
         });
-        jPanel1.add(L4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 110, 140, 130));
+        jPanel1.add(L4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 80, 140, 130));
 
         L5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/verificar.png"))); // NOI18N
+        L5.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         L5.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 L5MouseEntered(evt);
@@ -58,9 +65,10 @@ public class Menu_Principal extends javax.swing.JFrame {
                 L5MouseExited(evt);
             }
         });
-        jPanel1.add(L5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 250, 130, 130));
+        jPanel1.add(L5, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 220, 130, 130));
 
         L6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/busqueda.png"))); // NOI18N
+        L6.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         L6.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 L6MouseEntered(evt);
@@ -69,46 +77,66 @@ public class Menu_Principal extends javax.swing.JFrame {
                 L6MouseExited(evt);
             }
         });
-        jPanel1.add(L6, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 400, 130, 140));
+        jPanel1.add(L6, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 360, 130, 140));
 
         L7.setFont(new java.awt.Font("Traditional Arabic", 0, 18)); // NOI18N
-        jPanel1.add(L7, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 150, 180, 40));
+        jPanel1.add(L7, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 130, 180, 40));
 
         L8.setFont(new java.awt.Font("Traditional Arabic", 0, 18)); // NOI18N
-        jPanel1.add(L8, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 300, 180, 40));
+        jPanel1.add(L8, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 270, 180, 40));
 
         L9.setFont(new java.awt.Font("Traditional Arabic", 0, 18)); // NOI18N
-        jPanel1.add(L9, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 440, 220, 40));
+        jPanel1.add(L9, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 400, 220, 40));
 
-        jPanel2.setBackground(new java.awt.Color(102, 255, 255));
+        L10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/icons8_Multiply_32px.png"))); // NOI18N
+        L10.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        L10.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                L10MouseClicked(evt);
+            }
+        });
+        jPanel1.add(L10, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 10, 35, 33));
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 310, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 580, Short.MAX_VALUE)
-        );
+        L11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/icons8_Expand_Arrow_32px.png"))); // NOI18N
+        L11.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        L11.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                L11MouseClicked(evt);
+            }
+        });
+        jPanel1.add(L11, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 10, 40, 30));
 
-        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 0, 310, 580));
+        L13.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        L13.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/logo.png"))); // NOI18N
+        jPanel1.add(L13, new org.netbeans.lib.awtextra.AbsoluteConstraints(497, 70, 300, 150));
+
+        L14.setBackground(new java.awt.Color(255, 255, 255));
+        L14.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+        L14.setForeground(new java.awt.Color(255, 255, 255));
+        L14.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        L14.setText("VICTORITO HALLEY");
+        jPanel1.add(L14, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 220, 300, 50));
+
+        L12.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/City.png"))); // NOI18N
+        jPanel1.add(L12, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 0, 300, 500));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 791, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    /*
+    Eventos para cuano el mouse pase a travez de la etiqueta pase un texto
+    */
     private void L4MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_L4MouseEntered
         L7.setText("AÑADIR CLIENTE");
     }//GEN-LAST:event_L4MouseEntered
@@ -132,9 +160,15 @@ public class Menu_Principal extends javax.swing.JFrame {
     private void L6MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_L6MouseExited
         L9.setText("");
     }//GEN-LAST:event_L6MouseExited
+    //Evento para bajar la pestaña del menu principal
+    private void L11MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_L11MouseClicked
+        this.setState(Menu_Principal.ICONIFIED);
+    }//GEN-LAST:event_L11MouseClicked
+    //Evento para salir del programa
+    private void L10MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_L10MouseClicked
+       System.exit(0);
+    }//GEN-LAST:event_L10MouseClicked
 
-   
-     
     public static void main(String args[]) {
 
         try {
@@ -166,6 +200,11 @@ public class Menu_Principal extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel L1;
+    private javax.swing.JLabel L10;
+    private javax.swing.JLabel L11;
+    private javax.swing.JLabel L12;
+    private javax.swing.JLabel L13;
+    private javax.swing.JLabel L14;
     private javax.swing.JLabel L2;
     private javax.swing.JLabel L3;
     private javax.swing.JLabel L4;
@@ -175,6 +214,5 @@ public class Menu_Principal extends javax.swing.JFrame {
     private javax.swing.JLabel L8;
     private javax.swing.JLabel L9;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
